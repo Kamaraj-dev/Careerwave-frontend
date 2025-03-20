7f8d7ffb8d7707e846d0325dd0e625985b4a49e2
Kamaraj-dev <kamaraj7887@gmail.com>
2025-03-20 19:37:03 +0530

    auth add

--- src/Sercvices/AuthService.jsx
+++ src/Sercvices/AuthService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const base_url="http://localhost:8080/auth/"
+const base_url="https://careerwave-backend.onrender.com/auth/"
 
 const loginUser=async(login)=>{
     return axios.post(`${base_url}login`,login)
