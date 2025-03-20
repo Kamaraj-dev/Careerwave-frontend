87d06a87a1b75c5357e747fd6f3758bdfef211fb
Kamaraj-dev <kamaraj7887@gmail.com>
2025-03-20 13:10:46 +0530

    con backend

--- src/Interceptor/AxiosInterceptor.jsx
+++ src/Interceptor/AxiosInterceptor.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL:"https://careerwave-backend.onrender.com"
 });
 
 axiosInstance.interceptors.request.use(
